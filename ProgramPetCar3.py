4ebe584190a6ef64d17f3f7d2ae242d0d24eb55d
Ruth <66247272+rutheresia26@users.noreply.github.com>
2020-06-01 23:15:54 +0700

    Update ProgramPetCar3.py

--- ProgramPetCar3.py
+++ ProgramPetCar3.py
@@ -1,7 +1,7 @@
 iterasi = True
 def klinik () : 
     jumlah = int(input("Input Jumlah Hewan : "))
-    print ("Poliklinik Hewan 'Pet Car3' ")
+    print ("Poliklinik Hewan '8Care' ")
     print ("[1] : Konsultasi Dokter")
     print ("[2] : Vaksinasi")
     print ("[3] : lain-lain")
@@ -18,3 +18,23 @@ def klinik () :
         lain = 150000*jumlah
         print("Total biaya perawatan lain-lain : Rp.150000 per hewan")
         print("Total harga perawatan lain-lain : Rp.", lain)
+
+       
+def kucing () :
+    print ("Poliklinik Hewan '8Care' ")
+    print ("[1] : Konsultasi Dokter")
+    print ("[2] : Vaksinasi")
+    print ("[3] : lain-lain")
+    klk = input("Input jasa poliklinik :")
+    if klk == "1" :
+        hargakonsul = 150000*jumlah
+        print ("Harga konsultasi dokter per hewan : Rp.150000")
+        print ("Total harga konsultasi : Rp.", hargakonsul)
+    elif klk == "2" :
+        harga_vaksin = 80000*jumlah
+        print ("Harga vaksinasi : Rp.80000 per hewan")
+        print ("Total harga vaksinasi : Rp.", harga_vaksin)
+    elif klk == "3" :
+        lain = 120000*jumlah
+        print ("Total biaya perawatan lain-lain : Rp.120000 per hewan")
+        print ("Total harga perawatan lain-lain : Rp.", lain)
