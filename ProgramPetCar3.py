d052147a6fa73da32aacb9ab72bb6c34b54bfa99
Talithanaa <66210149+Talithanaa@users.noreply.github.com>
2020-06-05 15:40:34 +0700

    Update ProgramPetCar3.py

--- ProgramPetCar3.py
+++ ProgramPetCar3.py
@@ -43,17 +43,18 @@ def time1():
 def anjing():
     time1()
     print("Jenis hewan : Anjing")
+    jumlaha =int(input("Jumlah hewan:"))
     if selisih_tgl.days > 0 :
         if round(durasijam) > 0 :
-            bayar = ((selisih_tgl.days * 25000)*jumlah)
+            bayar = ((selisih_tgl.days * 25000)*jumlaha)
             print("Tarif inap per hewan : Rp.", (selisih_tgl.days*25000))
             print("Tarif total : Rp.", bayar)
         else :
-            bayar = ((selisih_tgl.days * 25000))*jumlah
+            bayar = ((selisih_tgl.days * 25000))*jumlaha
             print("Tarif per hewan : Rp.", selisih_tgl.days*25000)
             print("Tarif total : Rp ", bayar)
     else:
-        bayar =  (25000)*jumlah
+        bayar =  (25000)*jumlaha
         print("Tarif per hewan : Rp.", 25000)
         print("Tarif total : Rp ", bayar)
         
@@ -62,38 +63,41 @@ def anjing():
 def kucingnginep():
     time1()
     print("Jenis hewan : Kucing")
+    jumlahk = int(input("Jumlah hewan:"))
     if selisih_tgl.days > 0 :
         if round(durasijam) > 0 :
-            bayar = ((selisih_tgl.days * 20000)*jumlah)
+            bayar = ((selisih_tgl.days * 20000)*jumlahk)
             print("Tarif inap per hewan : Rp.", (selisih_tgl.days*20000))
             print("Tarif total : Rp.", bayar)
         else :
-            bayar = ((selisih_tgl.days * 20000))*jumlah
+            bayar = ((selisih_tgl.days * 20000))*jumlahk
             print("Tarif per hewan : Rp.", selisih_tgl.days*20000)
             print("Tarif total : Rp ", bayar)
     else:
-        bayar =  (20000)*jumlah
+        bayar =  (20000)*jumlahk
         print("Tarif per hewan : Rp.", 20000)
         print("Tarif total : Rp ", bayar)
         
             
 def klinik () : 
-    jumlah = int(input("Input Jumlah Hewan : "))
     print ("Poliklinik Hewan 'PetCare' ")
     print ("[1] : Konsultasi Dokter")
     print ("[2] : Vaksinasi")
     print ("[3] : lain-lain")
     klk=input("Input jasa poliklinik : ")
     if klk == "1" :
-        hargakonsul = 200000*jumlah
+        jumlah1 = int(input("Jumlah hewan:"))
+        hargakonsul = 200000*jumlah1
         print("Harga konsultasi dokter per hewan : Rp.200000")
         print("Total harga konsultasi : Rp.", hargakonsul)
     elif klk == "2" :
-        harga_vaksin = 100000*jumlah
+        jumlah2 = int(input("Jumlah hewan:"))
+        harga_vaksin = 100000*(jumlah2)
         print("Harga vaksinasi : Rp.100000 per hewan" )
         print("Total harga vaksinasi : Rp.", harga_vaksin)
     elif klk == "3" :
-        lain = int(input("Harga :"))
+        jumlah3 = int(input("Jumlah hewan:"))
+        lain = int(input("Harga :"))*(jumlah3)
         print("Total harga perawatan lain-lain : Rp.", lain)
 
        
@@ -104,11 +108,13 @@ def kucing () :
     print ("[3] : lain-lain")
     klk = input("Input jasa poliklinik :")
     if klk == "1" :
-        hargakonsul = 150000*jumlah
+        jumlah4 = int(input("Jumlah hewan:"))
+        hargakonsul = 150000*(jumlah4)
         print ("Harga konsultasi dokter per hewan : Rp.150000")
         print ("Total harga konsultasi : Rp.", hargakonsul)
     elif klk == "2" :
-        harga_vaksin = 80000*jumlah
+        jumlah5 = int(input("Jumlah hewan:"))
+        harga_vaksin = 80000*(jumlah5)
         print ("Harga vaksinasi : Rp.80000 per hewan")
         print ("Total harga vaksinasi : Rp.", harga_vaksin)
     elif klk == "3" :
@@ -123,16 +129,17 @@ def fasilitasA () :
     print ("[3] : Penitipan")
     fsl = input("Input fasilitas yang digunakan :")
     if fsl == "1" :
-        klinik()
-        
+        klinik()   
     elif fsl == "2" :
-            harga = 65000*jumlah
-            print("Harga Grooming Rp.65000 per hewan")
-            print("Total harga Grooming : Rp.", harga)
+        jumlah7=int(input("Jumlah hewan:"))
+        harga = 65000*jumlah
+        print("Harga Grooming Rp.65000 per hewan")
+        print("Total harga Grooming : Rp.", harga)
     elif fsl == "3" :
-            anjing ()
+        anjing ()
     else :
         print ("Tidak Tersedia")
+        
 def fasilitasK () :
     print ("List fasilitas yang tersedia:")
     print ("[1] : Klinik")
@@ -142,11 +149,12 @@ def fasilitasK () :
     if fsl == "1" :
          kucing ()
     elif fsl == "2" :
-            harga = 50000*jumlah
-            print("Biaya Grooming untuk kucing sebesar Rp.50000/kucing.")
-            print("Total biaya Grooming : Rp.", harga)
+        jumlah8 = int(input("Jumlah hewan:"))
+        harga = 50000*(jumlah8)
+        print("Biaya Grooming untuk kucing sebesar Rp.50000/kucing.")
+        print("Total biaya Grooming : Rp.", harga)
     elif fsl == "3" :
-            kucingnginep ()
+        kucingnginep ()
     else :
         print ("Tidak Tersedia")
         
@@ -162,7 +170,6 @@ print ("[2]: Kucing")
 jenis = int(input("Input Jenis Hewan : "))
 
 if jenis == 1:
-    jumlah = int(input("Input Jumlah Hewan : "))
     fasilitasA ()
     while iterasi:
         next = input("Tambah operasi lainnya untuk jenis hewan yang sama? (y/n) : ")
@@ -178,7 +185,6 @@ if jenis == 1:
             
       
 elif jenis == 2:
-    jumlah = int(input("Input Jumlah Hewan : "))
     fasilitasK ()
     while iterasi:
         next = input("Tambah operasi lainnya untuk jenis hewan yang sama? (y/n) : ")
